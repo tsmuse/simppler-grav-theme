16d3b16166f433221ab8078f01961bf25f04ad72
Tony Santos <tony@simppler.com>
2017-02-03 11:47:17 -0800

    fixed resize bug in nav bar. tightened up vertical spacing in homepage after all modules were styled to be responsive

--- js/pin_nav.js
+++ js/pin_nav.js
@@ -1,5 +1,22 @@
 var controller = new ScrollMagic.Controller();
 
 var scene = new ScrollMagic.Scene({triggerElement: "#nav-trigger", triggerHook: 0, duration: 0})
-                              .setPin("nav.nav-bar")
+                              //.setPin("nav.nav-bar")
+                              .on("enter", pin)
+                              .on("leave", unpin)
                               .addTo(controller);
+function pin(event){
+  // console.log("PInnnnnneed!!!");
+  $(".nav-bar").addClass("pinned");
+  $(".page-content").addClass("pinned-nav");
+  //select nav bar and apply a class that fixes it's position
+  //select page-content and apply a class that adjusts the padding to deal with the nav bar being yanked out of the flow
+}
+
+function unpin(event){
+  // console.log("UnnnPInnnnnneed!!!");
+  $(".nav-bar").removeClass("pinned");
+  $(".page-content").removeClass("pinned-nav");
+  //select nav bar and remove the pinned class
+  //select page-content and remove pinned class
+}
